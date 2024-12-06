7fa288fe301144bb7b09630d7d29718122b1a0a2
Pierre Chateau <pierre@chateau.se>
2024-12-07 00:41:58 +0100

    fixed Asset abstract class

--- week49/AssetTracker/Asset.cs
+++ week49/AssetTracker/Asset.cs
@@ -1,9 +1,52 @@
 ﻿
 internal abstract class Asset
 {
-    public abstract string Brand { get; }
-    public abstract string Model { get; }
-    public abstract Price PurchasePrice { get; }
-    public abstract DateTime PurchaseDate { get; }
-    public abstract Office Office { get; }
+    private Price _purchasePrice;
+    private DateTime _purchaseDate;
+    private string _model;
+    private string _brand;
+    private Office _office;
+
+    protected Asset(Price purchasePrice, DateTime purchaseDate, string brand, string model, string office)
+    {
+        _purchasePrice = new Price(purchasePrice.Cost, Currency.EUR);
+        _purchaseDate = purchaseDate;
+        _model = model;
+        _brand = brand;
+        _office = new Office(office, purchasePrice.Currency);
+    }
+
+    public string Brand 
+    {
+        get { return _brand; }
+    }
+
+    public string Model
+    {
+        get { return _model; }
+    }
+
+    public Price PurchasePrice
+    {
+        get
+        {
+            return _purchasePrice;
+        }
+    }
+
+    public DateTime PurchaseDate
+    {
+        get
+        {
+            return _purchaseDate;
+        }
+    }
+
+    public Office Office
+    {
+        get
+        {
+            return _office;
+        }
+    }
 }
--- week49/AssetTracker/Computer.cs
+++ week49/AssetTracker/Computer.cs
@@ -3,52 +3,7 @@ using AssetTracker;
 
 internal class Computer : Asset
 {
-    private Price _purchasePrice;
-    private DateTime _purchaseDate;
-    private string _model;
-    private string _brand;
-    private Office _office;
-
-    public Computer(Price purchasePrice, DateTime purchaseDate, string brand, string model, string office)
+    public Computer(Price purchasePrice, DateTime purchaseDate, string brand, string model, string office) : base(purchasePrice, purchaseDate, brand, model, office)
     {
-        _purchasePrice = new Price(purchasePrice.Cost,Currency.EUR);
-        _purchaseDate = purchaseDate;
-        _model = model;
-        _brand = brand;
-        _office = new Office(office,purchasePrice.Currency);
-    }
-
-    public override string Brand 
-    {
-        get { return _brand; }
-    }
-
-    public override string Model
-    {
-        get { return _model; }
-    }
-
-    public override Price PurchasePrice
-    {
-        get
-        {
-            return _purchasePrice;
-        }
-    }
-
-    public override DateTime PurchaseDate
-    {
-        get
-        {
-            return _purchaseDate;
-        }
-    }
-
-    public override Office Office
-    {
-        get
-        {
-            return _office;
-        }
     }
 }
